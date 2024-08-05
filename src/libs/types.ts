e3f0ebf81dd467ed8e3b693593fd738a2500def9
eye <eyenathaya5@gmail.com>
2024-08-05 23:43:39 +0700

    complete

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,3 +1,15 @@
 // add type for UserCard's Props
-
 // add type for UserCardDetail's Props
+interface UserCardProps{
+    name: string;
+    email: string;
+    imgUrl: string;
+    address: string;
+}
+export type{UserCardProps};
+
+interface UserCardDetailProps {
+    address: string;
+    email: string;
+  }  
+  export type { UserCardDetailProps};
